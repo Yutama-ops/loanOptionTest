20bc378f7b7f343cb44b1f5c20dfc0bd7104f2e7
tamcrush <tamcrush@yahoo.com>
2022-06-25 08:10:10 +1000

    first try
    try appliying simple html on react

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import data from './mock-data.json';
 
 function App() {
+  const [contacts, setContacts] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact)=> (
+            <tr>
+              <td>{contact.fullName}</td>
+              <td>{contact.address}</td>
+              <td>{contact.phoneNumber}</td>
+              <td>{contact.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h2>Add Contact</h2>
+      <form>
+        <input 
+          type="text"
+          name="fullName"
+          required="required"
+          placeholder='enter your name..'
+        />
+        <input 
+          type="text"
+          name="address"
+          required="required"
+          placeholder='enter your address'
+        />
+        <input 
+          type="text"
+          name="fullName"
+          required="required"
+          placeholder='enter your name..'
+        />
+      </form>
+
     </div>
   );
 }
