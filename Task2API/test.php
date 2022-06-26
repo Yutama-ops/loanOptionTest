5cca69e1b937b8894bbac1d298054997d9721355
tamcrush <tamcrush@yahoo.com>
2022-06-26 21:24:43 +1000

    task2
    task 2 completed

---
+++ Task2API/test.php
@@ -0,0 +1,33 @@
+
+
+    <?php
+    $result = array();
+function get_api($category, $limit){
+    $json = file_get_contents('https://api.publicapis.org/entries');
+    $object = json_decode($json, true);
+    $data = $object['entries'];
+
+    foreach($data as $dt){
+        if($dt['Category'] == $category){
+            $result[] = $dt['API'];
+            if(count($result) > $limit){
+                return $result;
+            }
+        }
+        // if($result != null){
+        //     array_push($result, $dt['API']);
+        // }
+        // return $dt['API'];
+    }
+    
+}
+$arr = get_api($argv[1], $argv[2]);
+if($arr != null){
+    rsort($arr);
+    foreach ($arr as $ar){
+        echo $ar."\n";
+    }
+} else {
+    echo 'No results';
+}
+?>
